0667079be1e946f36d447e8ce2d934a047d99b21
Sascha T <info@sascha-t.de>
2021-12-17 18:34:06 +0100

    Minimum Working Product

---
+++ readme.md
@@ -0,0 +1,8 @@
+# HTML Asset Downloader
+## API
+```
+let had = require("html-asset-downloader");
+await had.process("HTML FILE TO PROCESS", "FOLDER TO PUT ASSETS INTO");
+```
+## Install
+`npm install git+https://github.com/Sascha-T/html-asset-downloader.git`
---
+++ src/index.js
@@ -0,0 +1,42 @@
+// The Saschi HTML Localiser
+let fetch;
+const {writeFile, readFile, mkdir} = require("fs/promises");
+const {getExtension} = require('mime');
+const {join, relative, dirname} = require("path");
+const regex = /["'](https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=;]*))["']/g
+
+async function process(inputHTML, outputAssets) {
+    if(fetch == null)
+        fetch = (await import("node-fetch")).default;
+    await mkdir(outputAssets, {recursive: true});
+    let html = await readFile(inputHTML, "utf8");
+    let htmlDir = dirname(inputHTML);
+    let urls = [];
+    let match;
+    do {
+        match = regex.exec(html);
+        if (match)
+            urls.push(match[1]);
+    } while (match);
+    for (const i in urls) {
+        let url = urls[i];
+        let data = await fetch(url);
+        let rawData = Buffer.of(await data.arrayBuffer());
+
+        let header = data.headers.get("Content-Type");
+        let extension;
+        if(header != null)
+            extension = getExtension(header);
+        else
+            extension = url.split(/[#?]/)[0].split('.').pop().trim();
+        let outFile = join(outputAssets, i + "." + extension);
+        await writeFile(outFile, rawData);
+        html = html.replace(url, relative(htmlDir, outFile));
+    }
+    await writeFile(inputHTML, html);
+}
+module.exports = {
+    process
+}
+
+
